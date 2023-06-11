ffe297a864e5fce1179c72a0f28708bc3ac7279b
Ondřej Kavan <ondrej.kavan@gjkt.eu>
2023-06-11 19:51:21 +0200

    Final update - It's working !

--- maze_draw.py
+++ maze_draw.py
@@ -1,6 +1,11 @@
 from Translator import *
 from tkinter import *
 
+class Node:
+    def __init__(self, pos):
+        self.pos = pos
+        self.parent = None
+        self.around = []
 class Maze:
     def __init__(self):  
         self.canvas = None
@@ -10,10 +15,14 @@ class Maze:
         self.robot = None
         self.memory = []
         self.way = []
-        self.blind = []
+        self.end = None
+        self.queue = []
+        self.root = None
+        self.previous = None
         self.warning_txt = None
-        self.place_error = None
+        self.error_txt = None
         self.walked = []
+        self.lvl = None
         self.create_window()
 
     def create_window(self):
@@ -23,7 +32,7 @@ class Maze:
         self.window.title("Maze")
         self.canvas = Canvas(self.window, width = self.width, height = self.height)
         self.canvas.pack(side = LEFT)
-        self.start_program_button = Button(self.window, text = "Start program", bg = "red", command = self.call_way)
+        self.start_program_button = Button(self.window, text = "Start program", bg = "red", command = self.go_robot)
         self.start_program_button.place(x = 550, y = 300, width = 230, height = 100)
         x = Label(self.window, text = "x position")
         y = Label(self.window, text = "y position")
@@ -40,10 +49,12 @@ class Maze:
         self.lvl_button = Button(self.window, text = "Select Level", command = self.select_lvl)
         self.lvl_button.place(x = 560, y = 50, width = 210, height = 100)
         self.exit = Button(self.window, text = "Exit", command = self.window.destroy)
-        self.exit.place(x = 630, y = 420, width = 70, height = 20)
+        self.exit.place(x = 625, y = 420, width = 70, height = 20)
         self.window.mainloop()
             
     def create_robot(self):
+        if self.error_txt != None:
+            self.error_txt.destroy()
         #checks if you can place robot and create it
         if self.warning_txt != None:
             self.warning_txt.destroy()
@@ -61,33 +72,17 @@ class Maze:
             if self.lvl == Translator().return_maze(r"C:\Users\Administrator\Desktop\pogromovani\VSCode\maze_repository\LVL_1.txt"):
                 if self.lvl[y][x] == "0":
                     self.robot = self.canvas.create_oval((40*x)+200, (40*y)+120, 40*(x+1)+200, 40*(y+1)+120, fill = "blue")
-                elif self.lvl[y][x] == "1":
-                    self.warning_txt = Label(self.window, text = "Please, do not put player on walls", fg = "red")
-                    self.warning_txt.place(x=560,y=260, width = 200, height = 40) 
-                elif self.lvl[y][x] == "2":
-                    self.warning_txt = Label(self.window, text = "Do not choose the exit, please", fg = "red")
-                    self.warning_txt.place(x=560,y=260, width = 200, height = 40)
             elif self.lvl == Translator().return_maze(r"C:\Users\Administrator\Desktop\pogromovani\VSCode\maze_repository\LVL_2.txt"):
                 if self.lvl[y][x] == "0":
                     self.robot = self.canvas.create_oval((40*x)+130, (40*y)+80, 40*(x+1)+130, 40*(y+1)+80, fill ="blue")
-                elif self.lvl[y][x] == "1":
-                    self.warning_txt = Label(self.window, text = "Please, do not put player on walls", fg = "red")
-                    self.warning_txt.place(x=560,y=260, width = 200, height = 40)
-                elif self.lvl[y][x] == "2":
-                    self.warning_txt = Label(self.window, text = "Do not choose the exit, please", fg = "red")
-                    self.warning_txt.place(x=560,y=260, width = 200, height = 40)
             else:
                 if self.lvl[y][x] == "0":
-                    self.robot = self.canvas.create_oval((40*x)+50, (40*y)+30, 40*(x+1)+50, 40*(y+1)+30, fill = "blue")               
-                elif self.lvl[y][x] == "1":
-                    self.warning_txt = Label(self.window, text = "Please, do not put player on walls", fg = "red")
-                    self.warning_txt.place(x=560,y=260, width = 200, height = 40) 
-                elif self.lvl[y][x] == "2":
-                    self.warning_txt = Label(self.window, text = "Do not choose the exit, please", fg = "red")
-                    self.warning_txt.place(x=560,y=260, width = 200, height = 40)
+                    self.robot = self.canvas.create_oval((40*x)+50, (40*y)+30, 40*(x+1)+50, 40*(y+1)+30, fill = "blue")
                 
              
     def select_lvl(self):
+        if self.error_txt != None:
+            self.error_txt.destroy()
         #create lvl buttons
         self.canvas.delete("all")
         self.lvl = None
@@ -156,105 +151,126 @@ class Maze:
                         self.canvas.create_rectangle((40*x)+50, (40*y)+30, 40*(x+1)+50, 40*(y+1)+30,fill = "red")
                         x += 1
     
-    def call_way(self):
-        self.find_way([int(self.input_x.get())-1, int(self.input_y.get())-1],[int(self.input_x.get())-1, int(self.input_y.get())-1])
 
-    def find_way(self, position, start):
-        for i in range(len(self.lvl)):
-            for y in range(len(self.lvl[i])):
-                if self.lvl[i][y] == "2":
-                    self.end = [y,i]
-        x = position[0]
-        y = position[1]
-        begin_pos = start
-        if position == begin_pos:
-            self.memory.append(position)
-        around = []
-        if self.lvl[y][x] == "2":
-            pos = self.memory.pop()
-            while pos != begin_pos:
-                if pos not in self.blind:
-                    self.way.append(pos)
-                pos = self.memory.pop()
-            self.way.append(pos)
-            return self.go_robot()
-        for i in [-1,1]:
-                if x == 0 and i == -1 or x == len(self.lvl[y])-1 and i == 1:
+    def find_way(self, position):
+        for x in [1,-1]:
+            try:
+                if position.pos[0] == 0 and x == -1:
                     continue
-                if self.lvl[y][x+i] != "1" and [x+i,y] not in self.memory:
-                    around.append([x+i,y])           
-        for j in [-1,1]:
-            if y == 0 and j == -1 or y == len(self.lvl)-1 and j == 1:
+                elif self.lvl[position.pos[1]][position.pos[0]+x] != "1":
+                    way = Node([position.pos[0]+x, position.pos[1]])
+                    way.parent = position
+                    print(way.pos)
+                    if way.pos not in self.memory:
+                        self.memory.append(way.pos)
+                        self.queue.append(way)
+            except IndexError:
                 continue
-            elif self.lvl[y+j][x] != "1" and [x,y+j] not in self.memory:
-                around.append([x,y+j])
-        if around == []:
-            self.blind.append(position) 
-        for way in around:
-            if way not in self.memory:
-                self.memory.append(way)
-                self.find_way(way, begin_pos)
-
-    def walk(self,way):
-        x = way[0]
-        y = way[1]
-        prev_pos = self.walked.pop(0)
-        if self.lvl == Translator().return_maze(r"C:\Users\Administrator\Desktop\pogromovani\VSCode\maze_repository\LVL_1.txt"):
-            if prev_pos[0] == x:
-                x = 0
-                y = 1
-                self.canvas.move(self.robot,40*x, 40*y)
-                self.window.update()
-            elif prev_pos[1] == y:
-                y = 0
-                x = 1
-                self.canvas.move(self.robot, 40*x,40*y)
-                self.window.update()
-        elif self.lvl == Translator().return_maze(r"C:\Users\Administrator\Desktop\pogromovani\VSCode\maze_repository\LVL_2.txt"):
-            if prev_pos[0] == x:
-                x = 0
-                y = 1
-                self.canvas.move(self.robot, 40*x, 40*y)
-                self.window.update()
-            elif prev_pos[1] == y:
-                if x <= prev_pos[0]:
-                    x = -1
-                else:
-                    x = 1
-                y = 0
-                self.canvas.move(self.robot,40*x, 40*y)
-                self.window.update()
-        else:
-            if prev_pos[0] == x:
-                x = 0
-                y = 1
-                self.canvas.move(self.robot, 40*x, 40*y)
-                self.window.update()
-            elif prev_pos[1] == y:
-                if x <= prev_pos[0]:
-                    x = -1
-                else:
-                    x = 1
-                y = 0
-                self.canvas.move(self.robot, 40*x,40*y)
-                self.window.update()
+        for y in [1,-1]:
+            try:
+                if position.pos[1] == 0 and y == -1:
+                    continue
+                elif self.lvl[position.pos[1]+y][position.pos[0]] != "1":
+                    way = Node([position.pos[0],position.pos[1]+y])
+                    way.parent = position
+                    if way.pos not in self.memory:
+                        self.memory.append(way.pos)
+                        self.queue.append(way)
+            except IndexError:
+                continue
         
+        if self.lvl[position.pos[1]][position.pos[0]] == "2":
+            knot = position
+            self.way.append(knot.pos)
+            while knot.parent != None:
+                self.way.append(knot.parent.pos)
+                knot = knot.parent
+            return self
 
+            
+        
+    def find_end(self):
+        self.root = Node([int(self.input_x.get())-1,int(self.input_y.get())-1])
+        self.memory.append(self.root.pos)
+        self.queue.append(self.root)
+        while self.way == []:
+            self.find_way(self.queue.pop())
+        return self.way
+
+            
+    def walk(self):
+            try:
+                x = (self.place[0]-self.previous[0])*40
+                y = (self.place[1]-self.previous[1])*40
+                self.canvas.move(self.robot, x, y)
+                self.previous = self.place
+                self.place = self.way.pop()
+                x = None
+                y = None
+                self.window.after(600, self.walk)
+                self.window.update()
+            except IndexError:
+                x = (self.place[0]-self.previous[0])*40
+                y = (self.place[1]-self.previous[1])*40
+                self.canvas.move(self.robot, x, y)
+                self.window.update()
+                self.game_over()
+                
     def go_robot(self):
+        self.error_txt = None
         #disable buttons -> need to do
         self.start_program_button["state"] = DISABLED
         self.button["state"] = DISABLED
         self.exit["state"] = DISABLED
         self.lvl_button["state"] = DISABLED
         #checks if it is walkable -> need to do
-        self.way.reverse()
-        try:
-            while self.way[0][0] != self.end:
-                self.walked.append(self.way[0])
-                self.way.pop(0)
-                print(self.way)
-                self.window.after(1000, self.walk(self.way[0]))
-        except IndexError:
-            pass
+        if self.lvl != None:
+            if self.input_x.get() != "" and self.input_y.get() != "":
+                if self.lvl[int(self.input_y.get())-1][int(self.input_x.get())-1] != "1":
+                        try:
+                            self.way = self.find_end()
+                            self.previous = self.way.pop()
+                            self.place = self.way.pop()
+                            self.walk()
+                        except IndexError:
+                            self.error("Robot can't reach end")
+                else:
+                    self.error("Do not place robot on wall")
+            else:
+                self.error("Put both coordinates")
+        else:
+            self.error("select level")
+        
+    
+    def error(self, text):
+        if self.error_txt != None:
+            self.error_txt.destroy()
+        self.error_txt = Label(self.window, text = text)
+        self.error_txt.place(x = 560, y = 400, width = 200, height = 20)
+        self.start_program_button["state"] = NORMAL
+        self.exit["state"] = NORMAL
+        self.lvl_button["state"] = NORMAL
+        self.button["state"] = NORMAL
+
+    def game_over(self):
+        self.window.after(500, self.canvas.delete("all"))
+        self.window.update()
+        self.congrats = Label(self.window, text = "CONGRATULATIONS", fg = "green", font = "Helvetica 25 bold")
+        self.congrats.place(x = 80, y = 150, width = 400, height = 100)
+        self.play_again = Button(self.window, text = "Play again", command = self.play_again)
+        self.play_again.place(x = 225, y = 260, width = 100, height = 40)
+        self.exit_button = Button(self.window, text = "Quit game", command= self.window.destroy)
+        self.exit_button.place(x = 235, y = 310, width = 80, height = 40)
+
+    def play_again(self):
+        self.congrats.destroy()
+        self.play_again.destroy()
+        self.exit_button.destroy()
+        self.start_program_button["state"] = NORMAL
+        self.button["state"] = NORMAL
+        self.lvl_button["state"] = NORMAL
+        self.exit["state"] = NORMAL
+        self.input_x["state"] = NORMAL
+        self.input_y["state"] = NORMAL
 
 Maze()
